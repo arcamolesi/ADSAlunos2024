c5fe466e5678a3e02415ae1fab015540a9cb6cf5
Almir Rogério Camolesi <camolesi@fema.edu.br>
2024-08-23 21:03:13 -0300

    Add project files.

---
+++ Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ADSAlunos2024.Models
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Curso> Cursos { get; set; }
+
+
+    }
+}
---
+++ Models/Curso.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ADSAlunos2024.Models
+{
+    [Table("Cursos")]
+    public class Curso
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [StringLength(35)]
+        public string descricao { get; set; }
+
+    }
+}
