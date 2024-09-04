bf41c6cbf8913c743488eabed82662d4151ee6d1
Almir Rogério Camolesi <camolesi@fema.edu.br>
2024-09-03 22:49:04 -0300

    Atualização com Classe de Domínio Alunos.

---
+++ Migrations/20240904014407_Inicial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ADSAlunos2024.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    descricao = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: false),
+                    aniversario = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    cursoID = table.Column<int>(type: "int", nullable: false),
+                    periodo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Alunos_Cursos_cursoID",
+                        column: x => x.cursoID,
+                        principalTable: "Cursos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alunos_cursoID",
+                table: "Alunos",
+                column: "cursoID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alunos");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ADSAlunos2024.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {   
+        public enum Periodo { Manha, Tarde, Noite};
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name ="ID: ")]
+        public int id { get; set; }
+        
+        [Required(ErrorMessage = "Campo nome é obrigatório")]
+        [StringLength(35)]
+        [Display(Name = "Nome: ")]
+        public string nome { get; set; }
+
+        [Display(Name ="Data de Aniversário: ")]
+        public DateTime aniversario { get; set; }
+
+        [Display(Name ="Curso: ")]
+        public Curso curso { get; set; }
+        [Display(Name = "Curso: ")]
+        public  int cursoID { get; set; }
+
+        [Display(Name = "Período: ")]
+        public Periodo periodo { get; set; }
+
+    }
+}
